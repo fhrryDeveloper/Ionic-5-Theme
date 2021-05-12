390b0913fe02ec488e540041964d528ea613feb7
fhrryDeveloper <fhrrydeveloper@gmail.com>
2021-05-12 07:00:47 +0300

    Studied about SVG, slider pager and css variable

--- src/app/introduce/introduce.page.ts
+++ src/app/introduce/introduce.page.ts
@@ -1,11 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, ViewChild, AfterViewInit } from '@angular/core';
+import { IonSlides, MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-introduce',
   templateUrl: './introduce.page.html',
   styleUrls: ['./introduce.page.scss'],
 })
-export class IntroducePage implements OnInit {
-  constructor() { }
-  ngOnInit(){}
+export class IntroducePage implements AfterViewInit {
+
+  @ViewChild(IonSlides, { static: true }) slides: IonSlides;
+
+  @HostBinding('class.first-slide-active') isFirstSlide = true;
+
+  @HostBinding('class.last-slide-active') isLastSlide = false;
+
+  constructor(public menu: MenuController) { }
+  // Disable side menu for this page
+  ionViewDidEnter(): void {
+    this.menu.enable(false);
+  }
+
+  // Restore to default when leaving this page
+  ionViewDidLeave(): void {
+    this.menu.enable(true);
+  }
+
+  ngAfterViewInit(): void {
+    // ViewChild is set
+    this.slides.isBeginning().then(isBeginning => {
+      this.isFirstSlide = isBeginning;
+    });
+    this.slides.isEnd().then(isEnd => {
+      this.isLastSlide = isEnd;
+    });
+
+    // Subscribe to changes
+    this.slides.ionSlideWillChange.subscribe(changes => {
+      this.slides.isBeginning().then(isBeginning => {
+        this.isFirstSlide = isBeginning;
+      });
+      this.slides.isEnd().then(isEnd => {
+        this.isLastSlide = isEnd;
+      });
+    });
+  }
 }
